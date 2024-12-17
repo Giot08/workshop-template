50041495c78288ea76c0ae73d1f6c9c8c2b9573f
Giot08 <xgiot08@gmail.com>
2024-12-17 10:53:02 -0300

    Update tailwind config & AOS

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./theme/sections/*.{html,js}", "./theme/snippets/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
